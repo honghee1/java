620df3c1b8d4396c8ac32ae3929093ecf0f20fb5
honghee1 <103551705+honghee1@users.noreply.github.com>
2022-12-23 12:25:19 +0900

    Delete Step03 directory

--- Step03/src/main/java/config/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-public interface DBConfig {
-	public String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	public String DB_DRIVER = "oracle.jdbc.OracleDriver";
-	public String DB_USER = "ksw172";
-	public String DB_PASS = "tiger";
-}
